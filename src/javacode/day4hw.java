95aa938e9f89e6499cfc05e2df1ad3d48c400d2a
Kailas <kailasnadh456@gmail.com>
2026-08-11 10:50:59 +0530

    Add day4hw class for user authentication and role-based access control

---
+++ src/javacode/day4hw.java
@@ -0,0 +1,34 @@
+package javacode;
+import java.util.Scanner;
+
+public class day4hw {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter your username code");
+		String name=input.nextLine();
+		System.out.println("Enter your password");
+		int pass=input.nextInt();
+		System.out.println("Enter your roleid");
+		int roleid=input.nextInt();
+		if(name.equals("A")&&pass==1234&&roleid==1) {
+			System.out.println( "Welcome Admin. Full access granted.");
+		}
+		else if(name.equals("S")&&pass==1111&&roleid==2) {
+			System.out.println( "Welcome Student. Limited access granted.");
+			
+		}
+		else if(roleid==3) {
+			System.out.println( "Welcome Guest. View-only access." );
+		}
+		else {
+			System.out.println("Invalid credentials or role.");
+		}
+		
+		
+		
+		
+
+	}
+
+}
