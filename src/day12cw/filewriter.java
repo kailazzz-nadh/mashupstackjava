d2e95aa9a198b81636bc5c1703d97ef9d1a7039b
Kailas <kailasnadh456@gmail.com>
2026-08-22 11:31:17 +0530

    Add filewriter class for Java file handling; implement writing and reading from assignment.txt

---
+++ src/day12cw/filewriter.java
@@ -0,0 +1,41 @@
+package day12cw;
+import java.io.FileWriter;
+import java.io.FileReader;
+
+public class filewriter {
+
+	public static void main(String[] args) {
+
+		String data="Java File Handling Practice";
+		try {
+			
+		
+		FileWriter Write=new FileWriter("assignment.txt");
+		
+		 Write.write(data);
+		 
+	       System.out.println("Data is written to the file.");
+	 
+			
+Write.close();
+FileReader input = new FileReader("assignment.txt");
+
+char[] array = new char[100];
+int count = input.read(array);
+
+System.out.println("Data in the file:");
+System.out.println(array);
+
+input.close();
+	       
+	      
+	     }
+		 catch (Exception e) {
+		       e.getStackTrace();
+		     }
+		
+
+	}
+
+}
+
